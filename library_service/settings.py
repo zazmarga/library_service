3bf65136952007141ce4aa4ebbdb5c016956cf9f
Marharyta Zazulia <zazmargo@gmail.com>
2025-01-24 12:56:04 -0300

    initialize empty app: borrowing

--- library_service/settings.py
+++ library_service/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "borrowing",
 ]
 
 MIDDLEWARE = [
@@ -53,8 +54,7 @@ ROOT_URLCONF = "library_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
