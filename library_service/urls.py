b50d4b93e944149b464228185b6e34d9f35a7384
Marharyta Zazulia <zazmargo@gmail.com>
2025-01-25 12:55:19 -0300

    add path for list & detail endpoints

--- library_service/urls.py
+++ library_service/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("api/books/", include("books.urls"), name="books"),
     path("api/users/", include("user.urls", namespace="user")),
     path("api/borrowings/", include("borrowing.urls", namespace="borrowings")),
+    path("api/payments/", include("payments.urls", namespace="payments")),
 ]
