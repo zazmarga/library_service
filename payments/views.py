eb1a0e8bd124421ce71feedfc0355a2753f91986
Marharyta Zazulia <zazmargo@gmail.com>
2025-01-25 12:50:17 -0300

    add view list & detail of payments

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import (
+    PaymentSerializer,
+    PaymentRetrieveSerializer,
+)
+
+
+class PaymentView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Payment.objects.select_related("borrowing")
+    serializer_class = PaymentSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentRetrieveSerializer
+        return PaymentSerializer
